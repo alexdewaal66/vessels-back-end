ea724ac19e4e8aa7d683f3b3c590b1a041327f0f
alexdewaal66 <alexdewaal@gmail.com>
2021-05-03 12:37:55 +0200

    Anderhalfste commit: aantal bestanden toegevoegd

---
+++ src/test/java/nl/alexdewaal66/novi/vessels/VesselsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.alexdewaal66.novi.vessels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesselsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
