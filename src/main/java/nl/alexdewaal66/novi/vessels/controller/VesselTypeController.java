0b8d4befb16c6b97c18970ad225b27de07e64e93
alexdewaal66 <alexdewaal@gmail.com>
2021-08-06 11:49:19 +0200

    - added controller get endpoints

---
+++ src/main/java/nl/alexdewaal66/novi/vessels/controller/VesselTypeController.java
@@ -0,0 +1,26 @@
+package nl.alexdewaal66.novi.vessels.controller;
+
+import nl.alexdewaal66.novi.vessels.service.VesselTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/vesseltypes")
+@CrossOrigin(origins = "*")
+public class VesselTypeController {
+
+    @Autowired
+    private VesselTypeService vesselTypeService;
+
+    @GetMapping(value = "")
+    public ResponseEntity<Object> getVesselTypes() {
+        return ResponseEntity.ok().body(vesselTypeService.getVesselTypes());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> getVesselType(@PathVariable("id") long id) {
+        return ResponseEntity.ok().body(vesselTypeService.getVesselType(id));
+    }
+
+}
