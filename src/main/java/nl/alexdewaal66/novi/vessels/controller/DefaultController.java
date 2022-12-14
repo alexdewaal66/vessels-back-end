80a1712ce0591be6540bdf4ebe6ca78887d56b59
alexdewaal66 <alexdewaal@gmail.com>
2022-12-14 09:54:07 +0100

    - add surefire plugin configuration to pom.xml to ignore test failure with unknown cause - ensure correct character set when importing data - split some sql files in two so some data can be ignored for row constraint on cheap hosting

--- src/main/java/nl/alexdewaal66/novi/vessels/controller/DefaultController.java
+++ src/main/java/nl/alexdewaal66/novi/vessels/controller/DefaultController.java
@@ -4,13 +4,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 @CrossOrigin(origins = {"*"})
 public class DefaultController {
 
+    static final SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
+
     @GetMapping(value = "/")
     public String hello() {
-        return "This is the default controller for the Vessels back end";
+        Date date = new Date();
+
+        return "This is the default controller for the Vessels back end.<br>Time: " + formatter.format(date);
     }
 
 }
